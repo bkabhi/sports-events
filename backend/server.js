bd56f82f979eaea1a217bf219f6dedb97adbbc8f
bkabhi <bkabhijeet9801@gmail.com>
2023-01-02 05:08:51 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/', router)
+
 app.use('/',(req, res)=>{
   res.send("hello world!");
 })
-app.use('/', router)
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, async () => {
